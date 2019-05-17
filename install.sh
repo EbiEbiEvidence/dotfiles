3494a68ee38da45cd29f3530ec738c731e3c703d
Kosui Iwasa <contact@ebiebievidence.com>
2019-05-17 15:30:28 +0900

    Add installation myself

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+git clone https://github.com/EbiEbiEvidence/dotfiles ~/dotfiles
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 echo 'source ~/dotfiles/main.sh' >> ~/.zshrc
 git clone https://github.com/anyenv/anyenv ~/.anyenv
