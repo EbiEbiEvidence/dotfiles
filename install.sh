abe0096ad704b80e44b58dc3e5805454a61703dd
Kosui Iwasa <contact@ebiebievidence.com>
2019-05-17 15:40:27 +0900

    Bugfix

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 git clone https://github.com/EbiEbiEvidence/dotfiles ~/dotfiles
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 echo 'source ~/dotfiles/main.sh' >> ~/.zshrc
-git clone https://github.com/anyenv/anyenv ~/.anyenv
+source ~/.zshrc
 zplug install
+git clone https://github.com/anyenv/anyenv ~/.anyenv
+~/.anyenv/bin/anyenv init
+anyenv install --init
